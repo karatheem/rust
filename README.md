daaefe3442c9a065c0aed84200f0bd5413a97c16
karatheem <karatheem@gmail.com>
2024-06-21 18:08:56 +0300

    Initialization

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my journey in learning the Rust programming language. May it be fortuitous.
---
+++ hello_world/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world! I hope you're ready :D");
+    println!("also, Irina is beautiful!");
+}
+
+
